5e3721fa35d27b64000b71d30bd708a368c6edc3
Daria Rutts <darya.rutts@yandex.ru>
2023-08-24 15:54:31 +0300

    feat: добавить cors

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./middlewares/limiter');
 const router = require('./routes/index');
 const error = require('./middlewares/error');
+const allowedCors = require('./utils/allowedCors');
 
 const { PORT, DB_URL } = process.env;
 
 const app = express();
 
+app.use(cors({ origin: allowedCors, credentials: true }));
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
---
+++ utils/allowedCors.js
@@ -0,0 +1,4 @@
+module.exports = [
+  'http://localhost:4001',
+  'https://movies.dvr.nomoredomainsicu.ru',
+];
