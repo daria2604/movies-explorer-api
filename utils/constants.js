e31c15c9049c107f579946d10717112b9f5d38b3
Daria Rutts <darya.rutts@yandex.ru>
2023-10-28 17:01:11 +0300

    delete slash

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 const ALLOWED_CORS = [
   'http://localhost:4001',
   'https://movies.dvr.nomoredomainsicu.ru',
-  'https://dvr-movie-explorer.netlify.app/',
+  'https://dvr-movie-explorer.netlify.app',
 ];
 const PORT = process.env.PORT || 4000;
 const DB_URL = process.env.DB_URL || 'mongodb://127.0.0.1:27017/bitfilmsdb';
